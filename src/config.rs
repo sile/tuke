7b10fca75982cc7b2638715281d8feb42b1866f1
Takeru Ohta <phjgt308@gmail.com>
2025-12-12 21:51:40 +0900

    feat: add Action enum and key code parsing

--- src/config.rs
+++ src/config.rs
@@ -25,3 +25,29 @@ impl<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>> for Config {
         Ok(Self {})
     }
 }
+
+#[derive(Debug, Clone)]
+pub enum Action {
+    SendKey { code: tuinix::KeyCode },
+}
+
+impl<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>> for Action {
+    type Error = nojson::JsonParseError;
+
+    fn try_from(value: nojson::RawJsonValue<'text, 'raw>) -> Result<Self, Self::Error> {
+        let ty = value.to_member("type")?.required()?;
+        match ty.to_unquoted_string_str()?.as_ref() {
+            "send-key" => {
+                let code = parse_key_code(value.to_member("code")?.required()?)?;
+                todo!()
+            }
+            _ => Err(ty.invalid("unknown action type")),
+        }
+    }
+}
+
+fn parse_key_code(
+    value: nojson::RawJsonValue<'_, '_>,
+) -> Result<tuinix::KeyCode, nojson::JsonParseError> {
+    todo!()
+}
